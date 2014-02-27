74b26a0ee67aa412e0512d42cbcb9a0537fe7294
hashchange <heim@zeilenwechsel.de>
2014-02-27 14:32:23 +0100

    Updated jQuery paths in demo dir

--- demo/demo-amd/amd.js
+++ demo/demo-amd/amd.js
@@ -3,7 +3,7 @@ requirejs.config( {
     baseUrl: '../../bower_components',
 
     paths: {
-        'jquery': '../demo/bower_demo_components/jquery/jquery',
+        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
         'underscore': 'underscore/underscore',
         'backbone': 'backbone/backbone',
         'backbone.select': '/dist/amd/backbone.select'
