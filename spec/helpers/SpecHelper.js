5c8ad4ff693324d5d429420f918dda714fbf68d1
hashchange <heim@zeilenwechsel.de>
2015-03-02 15:55:14 +0100

    Fixed tests for Underscore 1.7.0

--- spec/helpers/SpecHelper.js
+++ spec/helpers/SpecHelper.js
@@ -106,7 +106,7 @@ _.extend( ListenerMixin.prototype, {
 
 ListenerMixin.applyTo = function ( hostObject, observableNames, takeSnapshotMethod ) {
     Backbone.Select.Me.applyTo( this );
-    _.extend( hostObject, new ListenerMixin( observableNames, takeSnapshotMethod ) );
+    deepExtend( hostObject, new ListenerMixin( observableNames, takeSnapshotMethod ) );
     hostObject.bindEvents();
 };
 
@@ -152,3 +152,20 @@ beforeEach( function () {
     } );
 
 } );
+
+// --- Other Utilities ---
+
+/**
+ * _.extend as in Underscore 1.6.0, copied verbatim. Used to work around the modified, shallow _.extend in
+ * Underscore 1.7.0.
+ */
+function deepExtend ( obj ) {
+    _.each( Array.prototype.slice.call( arguments, 1 ), function ( source ) {
+        if ( source ) {
+            for ( var prop in source ) {
+                obj[prop] = source[prop];
+            }
+        }
+    } );
+    return obj;
+}
