a870fa7adefbb87c41ed93dc777675caac2b8592
hashchange <heim@zeilenwechsel.de>
2016-09-25 00:36:04 +0200

    Added tests for auto-applying the Select.Me mixin

--- spec/helpers/fixture-utils.js
+++ spec/helpers/fixture-utils.js
@@ -5,53 +5,74 @@
  *
  * @typedef {Object} AutoCreationFixture
  *
- * @property {Object[]|undefined}         attributeSets   the attribute sets underlying the fixture, as passed in
- * @property {Object[]|undefined}         modelDataSets   the actual model data sets, calculated, differ from attribute
- *                                                        sets when testing parse
- * @property {Backbone.Model[]|undefined} models          an alternative to attributeSets, used when models are passe
- *                                                        in
- * @property {Function}                   Collection      the collection class to use, derived from the base class but
+ * @property {(Object[]|undefined)}       attributeSets   the attribute sets underlying the fixture, as passed in
+ *
+ * @property {(Object[]|undefined)}       modelDataSets   the actual model data sets, must be calculated from the
+ *                                                        attribute sets, must be different from them when testing parse
+ * @property {Backbone.Collection}        Collection      the collection class to use, derived from the base class but
  *                                                        modified as appropriate when the fixture is applied
- * @property {Function}                   modelTemplate   the model type the collection should use when generating
+ * @property {(Backbone.Model|undefined)} modelTemplate   the model type the collection should use when generating
  *                                                        models from data. When set to Backbone.Model, the
- *                                                        collection.model property remains undefined.
+ *                                                        collection.model property is left undefined.
  * @property {string}                     creationMethod  the operation which should create the models: "new" (models
- *                                                        are created during instantiation), "add", "remove", "reset"
+ *                                                        are created during instantiation), "add", "set", "reset"
  * @property {Object}                     options         the options applied during the operation which creates the
  *                                                        models
- *
- * @property {Function} createPopulatedCollection         predefined function; do not set this property. Creates (and
- *                                                        returns) the collection, and populates it according to the
- *                                                        chosen method.
  */
 
 /**
+ * Fixture constructor.
  *
- * @param {(Backbone.Model[]|Object[])} modelsOrAttributeSets  sets of models, or of attributes which the collection will
- *                                                             have to generate models from
+ * @param {Object[]} attributeSets  sets of attributes. Used either for feeding them to the collection as raw data to
+ *                                  generate models from, or for retrieving them, turned into a set of Backbone.Model
+ *                                  instances, with createPlainModels()
  * @constructor
  */
-function AutoCreationFixture( modelsOrAttributeSets ) {
-    if ( _.some( modelsOrAttributeSets, function ( item ) { return item && item instanceof Backbone.Model; } ) ) {
-        this.models = modelsOrAttributeSets;
-    } else {
-        this.attributeSets = modelsOrAttributeSets;
-    }
-
+function AutoCreationFixture( attributeSets ) {
+    this.attributeSets = attributeSets;
     this.options = {};
 }
 
+/**
+ * Sets the base collection type for the fixture.
+ *
+ * The actual fixture.Collection type will likely not be identical, but derived from it, extending the base type as
+ * needed.
+ *
+ * @param {Backbone.Collection} collectionType
+ */
 AutoCreationFixture.prototype.setBaseCollectionType = function ( collectionType ) {
     this.Collection = collectionType;
 };
 
+/**
+ * Creates and returns a set of Backbone.Models, generated from the initial attribute sets.
+ *
+ * @returns {Backbone.Model[]}
+ */
+AutoCreationFixture.prototype.createPlainModels = function () {
+    return _.map( this.attributeSets, function ( attributeSet ) {
+        return new Backbone.Model( attributeSet );
+    } );
+};
+
+/**
+ * Creates, populates and returns the collection.
+ *
+ * It is populated as defined in the creationMethod property (during instantiation, or with add, set, reset).
+ *
+ * @param   {(Object|Object[])}   modelData              can be raw data, or Backbone.Model instance(s)
+ * @param   {Object}              [instantiationOptions]
+ * @returns {Backbone.Collection}
+ */
 AutoCreationFixture.prototype.createPopulatedCollection = function ( modelData, instantiationOptions ) {
-    var collection;
+    var collection,
+        Collection = this.Collection;
 
     if ( this.creationMethod === "new" ) {
-        collection = new this.Collection( modelData, _.extend( {}, this.options, instantiationOptions ) );
+        collection = new Collection( modelData, _.extend( {}, this.options, instantiationOptions ) );
     } else {
-        collection = new this.Collection( null, instantiationOptions );
+        collection = new Collection( null, instantiationOptions );
         collection[this.creationMethod]( modelData, this.options );
     }
 
--- spec/selectMe.auto.creation.spec.js
+++ spec/selectMe.auto.creation.spec.js
@@ -1,6 +1,6 @@
 describe( 'Automatic creation of Select.Me models', function () {
 
-    var f,
+    var /** @type {AutoCreationFixture} */ f,
 
         SelectMeModel = Backbone.Model.extend( {
             tellType: "SelectMeModel",
@@ -243,12 +243,244 @@ describe( 'Automatic creation of Select.Me models', function () {
 
                         } );
 
-                    } )
+                    } );
 
-                } )
-            } )
+                } );
+
+            } );
+
         } );
 
+    } );
+
+    describe( 'Auto-Applying the Select.Me mixin on the fly, to Backbone models', function () {
+
+        beforeEach( function () {
+
+            f = new AutoCreationFixture( [
+                { number: 1 },
+                { number: 2 },
+                { number: 3 }
+            ] );
+
+        } );
+
+        describeWithData( collectionTypeScenarios, function ( configureCollectionType ) {
+
+            beforeEach( function () {
+                configureCollectionType( f );
+            } );
+
+            describeWithData( populationScenarios, function ( configurePopulation ) {
+
+                beforeEach( function () {
+                    configurePopulation( f );
+                } );
+
+                if( !hasOptedOut( configurePopulation, "singularItemTests" ) ) {
+
+                    describe( 'data for single item, not wrapped in an array, is passed in', function () {
+
+                        var collection, model;
+
+                        beforeEach( function () {
+                            var inputModel = f.createPlainModels()[0];
+                            collection = f.createPopulatedCollection( inputModel );
+                            model = collection.at( 0 );
+                        } );
+
+                        afterEach( function () {
+                            collection.close();
+                        } );
+
+                        it( 'the collection has the expected number of models', function () {
+                            expect( collection.length ).toEqual( 1 );
+                        } );
+
+                        it( 'the model is of the expected type', function () {
+                            expect( model ).toEqual( jasmine.any( Backbone.Model ) );
+                        } );
+
+                        it( 'the model has the expected attributes', function () {
+                            expect( model.attributes ).toEqual( f.attributeSets[0] );
+                            expect( model.get( "number" ) ).toEqual( 1 );
+                        } );
+
+                        it( 'the model has the Select.Me mixin applied', function () {
+                            expect( model._pickyType ).toEqual( "Backbone.Select.Me" );
+                        } );
+
+                        it( 'the model is selectable immediately after the collection mixin has been applied', function () {
+                            var firstSelectedItemInCollection,
+                                originalInitialize = collection.initialize,
+                                inputModel = f.createPlainModels()[0];
+
+                            f.Collection = f.Collection.extend( {
+                                initialize: function ( models, options ) {
+                                    originalInitialize.apply( this, arguments );
+
+                                    // When testing with models which are passed in during instantiation
+                                    if ( models ) models[0].select();
+
+                                    // When testing with models which are passed in during add, set or reset
+                                    this.listenTo( this, "add", function () { this.at(0).select() } );
+                                    this.listenTo( this, "set", function () { this.at(0).select() } );
+                                    this.listenTo( this, "reset", function () { this.at(0).select() } );
+                                }
+                            } );
+
+                            collection.close();
+                            collection = f.createPopulatedCollection( inputModel );
+
+                            if ( f.options.silent ) {
+                                // With options silent, we haven't been able to trigger an automatic selection after
+                                // "add"/"set"/"reset". We do it manually here, so see if things work, even though it is
+                                // stretching the concept of "immediately after the mixin is applied".
+                                collection.at( 0 ).select();
+                            }
+
+                            firstSelectedItemInCollection = collection._pickyType === "Backbone.Select.One" ? collection.selected : _.values( collection.selected )[0];
+
+                            expect( inputModel.selected ).toBe( true );
+                            expect( firstSelectedItemInCollection ).toBe( inputModel );
+                        } );
+
+                        if ( hasOptedIn( configurePopulation, "defaultLabelTest" ) ) {
+
+                            it( 'the model shares the defaultLabel setting of the collection', function () {
+                                // Only happens when creating the collection. Then, collection and models are
+                                // created in a single process, and the options passed to the collection are
+                                // used for both.
+                                var inputModel = f.createPlainModels()[0];
+                                collection.close();
+                                collection = f.createPopulatedCollection( inputModel, { defaultLabel: "foo" } );
+
+                                expect( collection.at( 0 )._pickyDefaultLabel ).toEqual( "foo" );
+                            } );
+
+                        }
+
+                        it( 'options are passed on to the the Select.Me mixin as it is applied', function () {
+                            // Testing with the defaultLabel option, the only option recognized by Select.Me.applyTo().
+                            var inputModel = f.createPlainModels()[0];
+
+                            _.extend( f.options, { defaultLabel: "foo" } );
+                            collection.close();
+                            collection = f.createPopulatedCollection( inputModel );
+
+                            expect( collection.at( 0 )._pickyDefaultLabel ).toEqual( "foo" );
+                        } );
+
+                    } );
+
+                }
+
+                describe( 'an array of item data is passed in', function () {
+
+                    var collection, models;
+
+                    beforeEach( function () {
+                        collection = f.createPopulatedCollection( f.createPlainModels() );
+                        models = collection.models;
+                    } );
+
+                    afterEach( function () {
+                        collection.close();
+                    } );
+
+                    it( 'the collection has the expected number of models', function () {
+                        expect( collection.length ).toEqual( 3 );
+                    } );
+
+                    it( 'the models are of the expected type', function () {
+                        expect( models[0] ).toEqual( jasmine.any( Backbone.Model ) );
+                        expect( models[1] ).toEqual( jasmine.any( Backbone.Model ) );
+                        expect( models[2] ).toEqual( jasmine.any( Backbone.Model ) );
+                    } );
+
+                    it( 'the models have the expected attributes', function () {
+                        expect( models[0].attributes ).toEqual( f.attributeSets[0] );
+                        expect( models[1].attributes ).toEqual( f.attributeSets[1] );
+                        expect( models[2].attributes ).toEqual( f.attributeSets[2] );
+                        expect( models[0].get( "number" ) ).toEqual( 1 );
+                        expect( models[1].get( "number" ) ).toEqual( 2 );
+                        expect( models[2].get( "number" ) ).toEqual( 3 );
+
+                    } );
+
+                    it( 'the models are selectable immediately after the collection mixin has been applied', function () {
+                        var firstSelectedItemInCollection,
+                            originalInitialize = collection.initialize,
+                            inputModels = f.createPlainModels();
+
+                        f.Collection = f.Collection.extend( {
+                            initialize: function ( models, options ) {
+                                originalInitialize.apply( this, arguments );
+
+                                // When testing with models which are passed in during instantiation
+                                if ( models ) models[0].select();
+
+                                // When testing with models which are passed in during add, set or reset
+                                this.listenTo( this, "add", function () { this.at(0).select() } );
+                                this.listenTo( this, "set", function () { this.at(0).select() } );
+                                this.listenTo( this, "reset", function () { this.at(0).select() } );
+                            }
+                        } );
+
+                        collection.close();
+                        collection = f.createPopulatedCollection( inputModels );
+
+                        if ( f.options.silent ) {
+                            // With options silent, we haven't been able to trigger an automatic selection after
+                            // "add"/"set"/"reset". We do it manually here, so see if things work, even though it is
+                            // stretching the concept of "immediately after the mixin is applied".
+                            collection.at( 0 ).select();
+                        }
+
+                        firstSelectedItemInCollection = collection._pickyType === "Backbone.Select.One" ? collection.selected : _.values( collection.selected )[0];
+
+                        expect( inputModels[0].selected ).toBe( true );
+                        expect( firstSelectedItemInCollection ).toBe( inputModels[0] );
+                    } );
+
+                    it( 'the models have the Select.Me mixin applied', function () {
+                        expect( models[0]._pickyType ).toEqual( "Backbone.Select.Me" );
+                        expect( models[1]._pickyType ).toEqual( "Backbone.Select.Me" );
+                        expect( models[2]._pickyType ).toEqual( "Backbone.Select.Me" );
+                    } );
+
+                    if ( hasOptedIn( configurePopulation, "defaultLabelTest" ) ) {
+
+                        it( 'the models share the defaultLabel setting of the collection', function () {
+                            // Only happens when creating the collection. Then, collection and models are
+                            // created in a single process, and the options passed to the collection are
+                            // used for both.
+                            collection.close();
+                            collection = f.createPopulatedCollection( f.createPlainModels(), { defaultLabel: "foo" } );
+                            expect( collection.at( 0 )._pickyDefaultLabel ).toEqual( "foo" );
+                            expect( collection.at( 1 )._pickyDefaultLabel ).toEqual( "foo" );
+                            expect( collection.at( 2 )._pickyDefaultLabel ).toEqual( "foo" );
+                        } );
+
+                    }
+
+                    it( 'options are passed on to the the Select.Me mixin as it is applied', function () {
+                        // Testing with the defaultLabel option, the only option recognized by Select.Me.applyTo().
+                        _.extend( f.options, { defaultLabel: "foo" } );
+
+                        collection.close();
+                        collection = f.createPopulatedCollection( f.createPlainModels() );
+
+                        expect( collection.at( 0 )._pickyDefaultLabel ).toEqual( "foo" );
+                        expect( collection.at( 1 )._pickyDefaultLabel ).toEqual( "foo" );
+                        expect( collection.at( 2 )._pickyDefaultLabel ).toEqual( "foo" );
+                    } );
+
+                } );
+
+            } );
+
+        } );
 
     } );
 
